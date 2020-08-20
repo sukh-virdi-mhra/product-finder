49d701ac9a1f2fcb501793714feddbea72915754
sukhvirdi7 <sukh.virdi2@mhra.gov.uk>
2020-08-20 17:10:44 +0100

    linking express to service

--- src/app-server.ts
+++ src/app-server.ts
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+import ProductFinder from "./services/finder"
+import getData from "./repositories/data-provider"
+let productFinder = new ProductFinder(getData);
+
 app.use(express.static("public"))
 
 const getHandler = (request, response) => {
   console.log("I'm listening!")
-  response.sendFile(__dirname + "/views/index.html")
+  response.render("index", {title: "pug", message: "pug message"})
 }
 
 app.get("/", getHandler)
@@ -15,10 +19,17 @@ app.get("/rereouted", (request, response) => {
     response.send("You have been rerouted!")
 })
 
-app.get("/mirror/:word", (request, response) => {
-    console.log(request.params.word)
-    response.send(request.parms.word)
-})
+app.get("/search/:products", (request, response) => {
+    let searchedProduct = productFinder.getProduct(request.params.products);
+    if (searchedProduct) {
+      response.send(searchedProduct);
+    } else {
+      response.send("Product Not Found");
+    }
+  });
+
+app.set("view engine", "pug")
+app.set("views", "./out/views");
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
